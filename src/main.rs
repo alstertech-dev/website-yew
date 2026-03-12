af8d8ef3b5f5f78823eddda2d5223e6ba7d5f128
innin-jam <innin.jam@gmail.com>
2026-03-12 19:56:17 +0100

    added gender enums for handling the german language (what am I wasting my time on...)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,57 @@
 use yew::prelude::*;
 
+const EMAIL: &str = "alstertech@alstergymnasium.de";
+
 #[derive(Clone, PartialEq)]
 pub struct Product {
     pub name: String,
     pub price: f32,
     pub image: String,
+    pub gender: grammar::Gender,
+    pub number: grammar::Number,
 }
+
+mod grammar {
+    #[derive(Clone, PartialEq)]
+    pub enum Gender {
+        Masculine,
+        Feminine,
+        Neutral,
+    }
+
+    #[derive(Clone, PartialEq)]
+    pub enum Number {
+        Singular,
+        Plural,
+    }
+
+    #[derive(Clone, PartialEq)]
+    pub enum Case {
+        Nominative,
+        Accusative,
+    }
+
+    pub fn get_definite_article(gender: Gender, number: Number, case: Case) -> String {
+        match number {
+            Number::Plural => "die",
+            Number::Singular => match gender {
+                Gender::Masculine => match case {
+                    Case::Nominative => "der",
+                    Case::Accusative => "den",
+                },
+                Gender::Feminine => match case {
+                    Case::Nominative | Case::Accusative => "die",
+                },
+                Gender::Neutral => match case {
+                    Case::Nominative | Case::Accusative => "das",
+                },
+            },
+        }
+        .to_owned()
+    }
+}
+
+use grammar::get_definite_article;
 
 #[derive(Properties, PartialEq)]
 pub struct Props {
@@ -18,9 +64,8 @@ fn product_card(props: &Props) -> Html {
     let on_click = {
         let product = props.product.clone();
         let on_click = props.on_click.clone();
-        move |_: MouseEvent| on_click.emit(product.clone())
+        Callback::from(move |_: MouseEvent| on_click.emit(product.clone()))
     };
-
     html! {
         <div class="product-card" onclick={on_click}>
             <img src={format!("/img/products/{}", &props.product.image)} alt={props.product.name.clone()} />
@@ -36,28 +81,34 @@ fn app() -> Html {
 
     let open_modal = {
         let selected = modal.clone();
-        move |product: Product| selected.set(Some(product))
+        Callback::from(move |product: Product| selected.set(Some(product)))
     };
     let close_modal = {
         let selected = modal.clone();
-        move |_| selected.set(None)
+        Callback::from(move |_| selected.set(None))
     };
 
     let products = vec![
         Product {
-            name: "Headphones".into(),
+            name: "Kopfhörer".into(),
             price: 79.10,
             image: "headphones.jpg".into(),
+            gender: grammar::Gender::Feminine,
+            number: grammar::Number::Plural,
         },
         Product {
-            name: "Camera Lense".into(),
+            name: "Linse".into(),
             price: 249.00,
             image: "lense.jpg".into(),
+            gender: grammar::Gender::Feminine,
+            number: grammar::Number::Plural,
         },
         Product {
             name: "Test Product".into(),
             price: 99.99,
             image: "t-rex.jpg".into(),
+            gender: grammar::Gender::Neutral,
+            number: grammar::Number::Singular,
         },
     ];
 
@@ -70,9 +121,19 @@ fn app() -> Html {
         </div>
         <div class={ if modal.is_some() { "modal-overlay open" } else { "modal-overlay" } } onclick={close_modal}>
             if let Some(product) = (*modal).clone() {
-                <div class="modal">
-                    <p class="modal-product-name">{ format!("{} -- €{:.2}", product.name, product.price) }</p>
-                </div>
+            <div class="modal" onclick={Callback::from(|e: MouseEvent| e.stop_propagation())}>
+                <p class="modal-product-name">{ format!("{} -- €{:.2}", product.name, product.price) }</p>
+                <p>
+                    {
+                        format!("Onlinebestellungen sind noch nicht verfügbar.
+                            Wenn Sie {} {} bestellen möchten, oder uns einfach eine Frage stellen wollen, schreiben Sie uns gerne eine Email an:
+                        ", get_definite_article(product.gender, product.number, grammar::Case::Accusative), product.name)
+                    }
+                </p>
+                <p class="modal-email">
+                {format!("{EMAIL}")}
+                </p>
+            </div>
             }
         </div>
         </>
